fbd645b5d49d9ef3c4fe3639443152d76ee88e45
Eric Couch <ericcouch@yahoo.com>
2020-09-23 11:28:05 -0500

    initial code for reading and writing to and from files

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace FS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // write to a file requires a filestream and a streamwriter
+            FileStream fs = new FileStream("test.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter textFile = new StreamWriter(fs);
+            textFile.Write("I changed this.  it should create a new file.");
+            textFile.Write("I'm adding this too!");
+            textFile.Close();
+
+            // read from a file requires a filestream and a streamreader
+            FileStream fr = new FileStream("testRead.txt", FileMode.Open, FileAccess.Read);
+            StreamReader textReader = new StreamReader(fr);
+            Console.WriteLine(textReader.ReadLine());
+            Console.WriteLine(textReader.ReadLine());
+            Console.WriteLine(textReader.ReadLine());
+            textReader.Close();
+        }
+    }
+}
